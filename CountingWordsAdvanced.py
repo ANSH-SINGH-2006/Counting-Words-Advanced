18c9fdb384f1a98a8ee2eb6761cba7972a0575af
Ansh Singh <64472289+ANSH-SINGH-2006@users.noreply.github.com>
2021-05-19 18:30:13 +0530

    Update CountingWordsAdvanced.py

--- CountingWordsAdvanced.py
+++ CountingWordsAdvanced.py
@@ -1,14 +1,10 @@
 x= input("Enter sentence: ")
 y=x.split()
 xff=0
-
+w=len(x.split())
 for w in y:
    p= w.split()
    w= len(p[0])
    xff=xff+w
-
-print("Total Words: ")
-w=len(x.split())
-print(w)
-print("Total Characters: ")
-print(xff)
+print("Total Words: "+w)
+print("Total Characters: "+xff)
