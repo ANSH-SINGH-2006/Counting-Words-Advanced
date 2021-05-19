91e2c80128be6a7fa550bb87277e3ce3766acdcc
Ansh Singh <64472289+ANSH-SINGH-2006@users.noreply.github.com>
2021-05-19 18:27:34 +0530

    Add files via upload

---
+++ CountingWordsAdvanced.py
@@ -0,0 +1,14 @@
+x= input("Enter sentence: ")
+y=x.split()
+xff=0
+
+for w in y:
+   p= w.split()
+   w= len(p[0])
+   xff=xff+w
+
+print("Total Words: ")
+w=len(x.split())
+print(w)
+print("Total Characters: ")
+print(xff)
